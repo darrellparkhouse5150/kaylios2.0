e59a4d4c4417ac6929f3679f5518f71f4817375f
darrellparkhouse5150 <86165927+darrellparkhouse5150@users.noreply.github.com>
2021-07-07 13:18:40 -0500

    added the is_following and follow functions to the follow_system

--- classes/follow_system.php
+++ classes/follow_system.php
@@ -1,10 +1,52 @@
 <?php
     class follow_system {
+        public $util;
+        public $notify;
+
         public function is_following($get) {
-            return true;
+            if (isset($_SESSION['id'])) {
+                $session = $_SESSION['id'];
+
+                $query = $this->db->prepare("SELECT follow_to from folllow_system where follow_by : session and follow_to=:get limit 1");
+                $query->execute([':session' => $session, ':get' => $get]);
+
+                if ($query->rowCount() != 0 || $query->rowCount() != null) {
+                    return true;
+                } else if ($query->rowCount() == 0) {
+                    return  false;
+                }
+            }
+        }
+
+        public function follow($get) {
+            $this->util = new util();
+            $this->notify = new notify();
+            $this->settings = new settings();
+
+            $session = $_SESSION['id'];
+            $user_session = $this->util->getDetails($session, 'username');
+            $get_user = $this->util->getDetails($get, 'username');
+
+            if ($this->settings->amIBlockd($get) == false) {
+                if ($this->is_following($get) == false) {
+                    $query = $this->db->prepare('INSERT INTO follow_system(follow_by, follow_by_username, follow_to, follow_to_username, time)
+                                                values (:session, :user_session, :get, :get_user, now())');
+                    $query->execute([':session' => $session, ':user_session' => $user_session, ':get_user' => $get_user]);
+
+                    $this->notify->followNotification($get, "follow");
+
+                    return "ok";
+                } else {
+                    return "Already followed";
+                }
+            }
+
         }
 
         public function MeOrNot($get) {
             
         }
     }
+
+    $fs = new follow_system();
+    $fs->is_following('');
